bd587fc98abc3dbda1d9f14aa84ecb3b363ed9a6
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-01-03 02:31:57 +0300

    Add FacesApiTest console app

---
+++ FacesApiTest/ImageUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacesApiTest
+{
+    public class ImageUtility
+    {
+        public byte[] ConvertToBytes(string imagePath)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            using (var stream = new FileStream(imagePath, FileMode.Open))
+            {
+                stream.CopyTo(memoryStream);
+            }
+            return memoryStream.ToArray();
+        }
+
+        public void SaveToFile(byte[] imageBytes, string imagePath)
+        {
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                Image img = Image.FromStream(ms);
+                img.Save(imagePath, ImageFormat.Jpeg);
+            }
+        }
+    }
+}
---
+++ FacesApiTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FacesApiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var imagePath = @"oscars-2017.jpg";
+            var urlAddress = @"http://localhost:6000/api/faces";
+            var utils = new ImageUtility();
+            var bytes = utils.ConvertToBytes(imagePath);
+            List<byte[]> faces = null;
+            var byteContent = new ByteArrayContent(bytes);
+            byteContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.PostAsync(urlAddress, byteContent))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    faces = JsonSerializer.Deserialize<List<byte[]>>(apiResponse);
+                }
+            }
+            for (int i = 0; i < faces.Count; i++)
+            {
+                utils.SaveToFile(faces[i], Path.Combine(Directory.GetCurrentDirectory(), $"face{i}.jpg"));
+            }
+        }
+    }
+}
