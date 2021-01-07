ca02b8d2ebd2a4436d82e7944253f487420bc898
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-01-07 15:11:25 +0300

    Add MS SQL Server dependency

--- BuildFramework/Install-Deps.ps1
+++ BuildFramework/Install-Deps.ps1
@@ -2,3 +2,4 @@ Write-Host Installing RabbitMq
 minikube profile facesystem
 helm repo add center https://repo.chartcenter.io
 helm install rabbitmq center/bitnami/rabbitmq
+helm install mssql stable/mssql-linux --set acceptEula.value=Y --set edition.value=Developer
