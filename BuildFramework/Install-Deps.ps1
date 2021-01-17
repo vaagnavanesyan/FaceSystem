9c1b2959d8df75ef559f302f016957bf385e7580
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-01-17 15:27:14 +0300

    Add default credentials for dependencies

--- BuildFramework/Install-Deps.ps1
+++ BuildFramework/Install-Deps.ps1
@@ -1,5 +1,5 @@
 Write-Host Installing RabbitMq
 minikube profile facesystem
 helm repo add center https://repo.chartcenter.io
-helm install rabbitmq center/bitnami/rabbitmq
-helm install mssql stable/mssql-linux --set acceptEula.value=Y --set edition.value=Developer
+helm install rabbitmq center/bitnami/rabbitmq --set auth.username=user --set auth.password=bX1DTrlOfH
+helm install mssql stable/mssql-linux --set acceptEula.value=Y --set edition.value=Developer --set sapassword=bX1DTrlOfH
