b66b5177d0f694767b77e4bc7bd6e4392d9cb0f8
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-01-22 21:39:11 +0300

    Add OrdersController

---
+++ Ordering.OrdersApi/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.OrdersApi.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.OrdersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public OrdersController(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var orders = await orderRepository.GetOrdersAsync();
+            return Ok(orders);
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<IActionResult> GetOrderById(Guid orderId)
+        {
+            var order = await orderRepository.GetOrderAsync(orderId);
+            if(order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+    }
+}
