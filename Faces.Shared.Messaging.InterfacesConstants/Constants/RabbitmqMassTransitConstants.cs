2aee0f934d6d2f876bc14ffe8c04b92fa0e0992f
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-01-23 13:11:11 +0300

    Add constants for Notification commands

--- Faces.Shared.Messaging.InterfacesConstants/Constants/RabbitmqMassTransitConstants.cs
+++ Faces.Shared.Messaging.InterfacesConstants/Constants/RabbitmqMassTransitConstants.cs
@@ -6,5 +6,7 @@
         public const string UserName = "user";
         public const string Password = "bX1DTrlOfH";
         public const string RegisterOrderCommandQueue = "register.order.command";
+        public const string NotificationServiceQueue = "notification.service.queue";
+        public const string OrderDispatchedServiceQueue = "order.dispatch.service.queue";
     }
 }
