48ee4f2d3f2b923aad2b37e844e8b34a926ce445
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-01-23 13:16:14 +0300

    Add EmailService

---
+++ Notifications.EmailService/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications.EmailService
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(Message message);
+    }
+
+}
